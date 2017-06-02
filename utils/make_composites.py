d1265cbd1c91dcde8a4854119a6d0adf9df50148
bitcraft <leif.theden@gmail.com>
2017-06-02 08:26:29 -0500

    linting

--- utils/make_composites.py
+++ utils/make_composites.py
@@ -12,62 +12,69 @@ from tailor.builder import YamlTemplateBuilder
 from tailor.plugins.composer.renderer import TemplateRenderer
 
 
+def double(image, filename):
+    base = Image.new('RGBA', (1200, 1800))
+    areas = ((0, 0), (600, 0))
+    for area in areas:
+        base.paste(image, area, mask=image)
+    base.save(filename)
+
+
 def normpath(path):
     path = os.path.expanduser(path)
     path = os.path.normpath(path)
     return path
 
 
-@asyncio.coroutine
-def render_all_files(template, files):
+async def render_all_files(template, files):
     renderer = TemplateRenderer()
 
-    filename_queue = asyncio.JoinableQueue()
-    image_queue = asyncio.JoinableQueue()
+    filename_queue = asyncio.Queue()
+    image_queue = asyncio.Queue()
 
     create_task = asyncio.get_event_loop().create_task
 
     tasks = set()
     tasks.add(create_task(async_queue_filenames(filename_queue, files)))
     tasks.add(create_task(async_consume_filenames(filename_queue, image_queue)))
-    tasks.add(
-        create_task(async_consume_images(template, renderer, image_queue)))
+    tasks.add(create_task(async_consume_images(template, renderer, image_queue)))
     tasks.add(filename_queue.join())
 
-    yield from asyncio.wait(tasks)
+    await asyncio.wait(tasks)
 
 
-@asyncio.coroutine
-def async_consume_images(template_master, renderer, image_queue):
+async def async_consume_images(template_master, renderer, image_queue):
     index = 0
 
-    @asyncio.coroutine
-    def get():
-        image = yield from image_queue.get()
+    async def get():
+        image = await image_queue.get()
         if image is None:
-            raise
+            raise ValueError
         return image
+
+
 
     while 1:
         template = copy.deepcopy(template_master)
 
-        image0 = yield from get()
-        image1 = yield from get()
-        image2 = yield from get()
-        image3 = yield from get()
+        image0 = await get()
+        image1 = await get()
+        image2 = await get()
+        image3 = await get()
 
         template.push_image(image0)
         template.push_image(image1)
         template.push_image(image2)
         template.push_image(image3)
 
-        filename = 'composite' + str(index) + '.png'
-        yield from renderer.render_all_and_save(template, filename)
+        filename = 'session-melissa-robbie-{:05d}.jpg'.format(index)
+        image = await renderer.render_all(template)
+        double(image, filename)
+        print(filename)
         index += 1
 
 
-@asyncio.coroutine
-def async_consume_filenames(filename_queue, image_queue):
+async def async_consume_filenames(filename_queue, image_queue):
     create_task = asyncio.get_event_loop().create_task
 
     def task_done(future):
@@ -75,7 +82,7 @@ def async_consume_filenames(filename_queue, image_queue):
         filename_queue.task_done()
 
     while 1:
-        filename = yield from filename_queue.get()
+        filename = await filename_queue.get()
         if filename is None:
             filename_queue.task_done()
             break
@@ -83,29 +90,54 @@ def async_consume_filenames(filename_queue, image_queue):
         task.add_done_callback(task_done)
 
 
-@asyncio.coroutine
-def async_queue_filenames(filename_queue, files):
-    # assert(len(files) % 4 == 0)
-    for index, filename in enumerate(files):
-        yield from filename_queue.put(filename)
-    yield from filename_queue.put(None)
+async def async_queue_filenames(filename_queue, files):
+    assert (len(files) % 4 == 0)
+    for index, filename in enumerate(sorted(files)):
+        await filename_queue.put(filename)
+    await filename_queue.put(None)
 
 
-@asyncio.coroutine
-def async_open(filename):
-    image = yield from loop.run_in_executor(None, Image.open, filename)
+async def async_open(filename):
+    image = await loop.run_in_executor(None, Image.open, filename)
     return image
+
+
+async def main():
+    template_filename = normpath('tailor/resources/templates/standard.yaml')
+    template_master = YamlTemplateBuilder().read(template_filename)
+    folder = normpath(os.path.join("z:", "Dropbox", "photob", "erin-drew"))
+    files = glob.glob('{0}/*.jpg'.format(folder))
+    assert files
+    files.sort()
+
+    renderer = TemplateRenderer()
+
+    index = 0
+    while files:
+        template = copy.deepcopy(template_master)
+
+        image0 = Image.open((files.pop(0)))
+        image1 = Image.open((files.pop(0)))
+        image2 = Image.open((files.pop(0)))
+        image3 = Image.open((files.pop(0)))
+
+        template.push_image(image0)
+        template.push_image(image1)
+        template.push_image(image2)
+        template.push_image(image3)
+
+        filename = 'session-erin-drew-{:05d}.jpg'.format(index)
+        image = await renderer.render_all(template)
+        double(image, filename)
+        print(filename)
+        index += 1
+
+        print(files)
 
 
 if __name__ == "__main__":
     import time
 
-    start = time.time()
+    import asyncio
     loop = asyncio.get_event_loop()
-    template_filename = normpath(
-        'tailor/resources/templates/standard.yaml')
-    template_master = YamlTemplateBuilder().read(template_filename)
-    folder = normpath('~/events/carrie-jon/originals/')
-    files = glob.glob('{0}/*.jpg'.format(folder))
-
-    loop.run_until_complete(render_all_files(template_master, files))
+    loop.run_until_complete(main())
